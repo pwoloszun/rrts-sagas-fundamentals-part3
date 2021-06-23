0b8daf410dc3cf01cb6c9882f7070efd77a2e902
pwoloszun <piotr.woloszun@gmail.com>
2021-06-23 15:41:15 +0200

    dec p1

--- src/store/22b-asyncCounterWithSaga/asyncCounterWithSagaSlice.ts
+++ src/store/22b-asyncCounterWithSaga/asyncCounterWithSagaSlice.ts
@@ -31,6 +31,16 @@ export const asyncCounterSlice = createSlice({
       state.isLoading = false;
       state.asyncWithSagaValue = value;
     },
+
+    decrementRequest: (state, action: PayloadAction<{ decBy: number }>) => {
+      state.isLoading = true;
+    },
+
+    decrementSuccess: (state, action: PayloadAction<{ value: number }>) => {
+      const { value } = action.payload;
+      state.isLoading = false;
+      state.asyncWithSagaValue = value;
+    },
   },
 });
 
