2364f5b7fc7bde3aaa76b5acac0c36ba6cb60181
pwoloszun <piotr.woloszun@gmail.com>
2021-06-30 14:14:34 +0200

    async p2

--- src/features/22-redux-async/containers/AsyncCounterUsingSaga/index.tsx
+++ src/features/22-redux-async/containers/AsyncCounterUsingSaga/index.tsx
@@ -1,13 +1,20 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { selectors, actions } from 'src/store/22b-asyncCounterWithSaga';
+
 import Counter from '../../../../components/Counter';
 
 export default function AsyncCounterUsingSaga() {
-  const value = 456;
-  const isLoading = true;
+  const value = useSelector(
+    selectors.selectAsyncCounterWithSagaValue
+  );
+  const isLoading = useSelector(
+    selectors.selectIsLoading
+  );
+  const onIncrementHandler = () => {
 
-  const onIncrementHandler = () => { };
+  };
 
   return (
     <div>
